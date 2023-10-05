a84a7757da2747a259beee6438b6bacfb8e8984a
Hadhemi33 <benmansourhadhemi01@gmail.com>
2023-10-05 22:31:42 +0100

    connect wallet starting

---
+++ pages/connectWallet.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+
+import Style from "../styles/connectWallet.module.css";
+import images from "../img";
+const connectWallet = () => {
+  return <div>connectWallet</div>;
+};
+
+export default connectWallet;
