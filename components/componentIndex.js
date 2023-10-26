2156372c6152bb18a66fbc200de54c58fff41b50
ApocalyptoRun <diengibrahima2222@gmail.com>
2023-10-26 13:31:27 +0100

    BigNFTSlider

--- components/componentIndex.js
+++ components/componentIndex.js
@@ -3,5 +3,6 @@ import Footer from './Footer/Footer'
 import NavBar from './NavBar/NavBar'
 import HeroSection from './HeroSection/HeroSection'
 import Service from './ServiceComponent/Service'
+import BigNFTSlider from './BigNFTSlider/BigNFTSlider'
 
-export { Button, Footer, NavBar, HeroSection, Service };
+export { Button, Footer, NavBar, HeroSection, Service, BigNFTSlider };
