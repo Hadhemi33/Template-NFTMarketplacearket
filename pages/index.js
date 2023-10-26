71726b1ad2118051e50f2de19e820b8821eee5ca
ApocalyptoRun <diengibrahima2222@gmail.com>
2023-10-26 11:54:30 +0100

    Servise Section

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import React from "react";
 
 //INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection } from "../components/componentIndex";
+import { HeroSection, Service } from "../components/componentIndex";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
+      <Service />
     </div>
   );
 };
